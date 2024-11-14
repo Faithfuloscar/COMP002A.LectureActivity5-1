fcbd909b489e9ac131e490a9e1707b7308492b2e
Faithfuloscar <oscarmendez2003@outlook.com>
2024-11-13 17:59:53 -0800

    lecture activity is completed

--- Script.js
+++ Script.js
@@ -1,5 +1,4 @@
   // myGrades is not defined. Added let to define myGrades
-  "use strict"
  let myGrades = [100, 100, 90, 73, 78, 94, 86];
  // myAverage is not defined. Added let to define myAverage
  let myAverage = 0;
@@ -10,3 +9,15 @@
 
     myAverage = myAverage / myGrades.length;
     console.log("My grade average is " + myAverage);
+
+    "use strict"
+    let myNumber = "Orange";
+    
+    if (myNumber >= 0 && myNumber <= 100) {
+        console.log("Your number is " + myNumber)
+
+    }
+
+    else {
+        console.log("system 32 will now be deleted");
+    }
