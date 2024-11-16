37b92ed2251b34458620ac351829740c0d63d854
Faithfuloscar <oscarmendez2003@outlook.com>
2024-11-16 14:18:32 -0800

    updated and completed the activity properly

--- Script.js
+++ Script.js
@@ -3,21 +3,75 @@
  // myAverage is not defined. Added let to define myAverage
  let myAverage = 0;
     
-    for (let i = 1; i <= myGrades.length; i++) {
-    myAverage += myGrades[i];
+ // The loop skipped first index in the array, solution set i=0 
+    for (let i = 0; i < myGrades.length; i++) {
+    myAverage += myGrades[i]; // adds each grade to the total.
 }
 
-    myAverage = myAverage / myGrades.length;
-    console.log("My grade average is " + myAverage);
+    myAverage = myAverage / myGrades.length; // divides the total by the number of grades
 
-    "use strict"
-    let myNumber = "Orange";
+    console.log("My grade average is " + myAverage); // outputs average
+
+    // function to validate a grade input
+    function validateNumber(number) {
+        // checks if input is a number
+        if(typeof number != 'number') {
+            throw new Error("input must be a number");
+        }
+
+        // checks if input is less than 0
+        if (number < 0){
+            throw new Error("number cannot be less than 0");
+        }
+
+        // checks if input is greater than 100
+        if (number > 100){
+            throw new Error("number cannot be greater than 100")
+        }
+
+        console.log("Valid number: " + number);
+    }
+
+    // custom exception class
+    class NumberValidationException extends Error{
+        constructor(message) {
+            super(message);
+            this.name ="Number ValidationException"
+        }
+    }
+
+    // function to validate a grade input
+    function validateNumber(number) {
+    // checks if input is a number
+    if(typeof number != 'number') {
+        throw new NumberValidationException("input must be a number");
+    }
     
-    if (myNumber >= 0 && myNumber <= 100) {
-        console.log("Your number is " + myNumber)
-
+    // checks if input is less than 0
+    if (number < 0){
+        throw new NumberValidationException("number cannot be less than 0");
     }
-
-    else {
-        console.log("system 32 will now be deleted");
+    
+    // checks if input is greater than 100
+    if (number > 100){
+        throw new NumberValidationException("number cannot be greater than 100")
     }
+    
+    console.log("Valid number: " + number);
+}
+
+try {
+    validateNumber(110);
+} catch(error) {
+    if (error instanceof NumberValidationException){
+        console.error("Number Validation Error: " + error.message);
+    } else {
+        console.error("Unexpected Error: " + error.message);
+    }
+}
+
+try {
+    validateNumber(23);
+} catch (error){
+    console.Error(error.message);
+}
